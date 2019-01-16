71f22afe51f9fd0b5711c6d05ffdc56f8d0ab479
Tomoki108 <shinonome81@gmail.com>
2019-01-17 03:06:48 +0900

    two models are fixed

--- app/LessonWord.php
+++ app/LessonWord.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Lesson_word extends Model
+class LessonWord extends Model
 {
     //
 }
--- app/WordAnswer.php
+++ app/WordAnswer.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Word_answer extends Model
+class WordAnswer extends Model
 {
     //
 }
