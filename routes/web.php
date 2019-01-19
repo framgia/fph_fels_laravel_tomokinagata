de360d3fd37e93d0e7a4696b5cf9327fe18d5838
Tomoki108 <shinonome81@gmail.com>
2019-01-19 13:03:55 +0900

    create dashboard front

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//temporary
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
