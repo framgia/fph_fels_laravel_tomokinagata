29dd78e6c77bf4f6437ab368f33197d44300f1e1
Tomoki108 <shinonome81@gmail.com>
2019-01-24 00:35:17 +0900

    set maxlength for inputs

--- resources/views/admin/addCategory.blade.php
+++ resources/views/admin/addCategory.blade.php
@@ -11,9 +11,9 @@
             {{csrf_field()}}
             <h3 class="mb-3">Add category</h3>
             <h5>Title</h5>
-            <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }} mb-4 col-12" name="title" value="{{ old('title') }}" required>
+            <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }} mb-4 col-12" name="title" value="{{ old('title') }}" maxlength="20" required>
             <h5>Description</h5>
-            <textarea id="description" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }} mb-4 col-12" name="description" rows="8" required>{{ old('description') }}</textarea>
+            <textarea id="description" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }} mb-4 col-12" name="description" rows="8" maxlength="200" required>{{ old('description') }}</textarea>
             <button type="submit" class="btn btn-primary w-100">Submit</button> 
         </form>
     <div>
--- resources/views/admin/editCategory.blade.php
+++ resources/views/admin/editCategory.blade.php
@@ -14,11 +14,9 @@
         {{csrf_field()}}
         <h3 class="mb-5">Edit category</h3>
         <h5>Title</h5>
-        <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }} mb-4 col-12" name="title" value="{{ old('title', $category->title) }}"  required>
-
+        <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }} mb-4 col-12" name="title" value="{{ old('title', $category->title) }}" maxlength="20" required>
         <h5>Description</h5>
-        <textarea id="description" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }} mb-4 col-12" name="description" rows="8" required>{{ old('description', $category->description) }}</textarea>
-        
+        <textarea id="description" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }} mb-4 col-12" name="description" rows="8" maxlength="200" required>{{ old('description', $category->description) }}</textarea>
         <button type="submit" class="btn btn-primary w-100">Submit</button> 
     </form>
   <div>
