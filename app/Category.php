7e738b714822c871b3786b021d946ecfb551dfb9
Tomoki Nagata <45285704+Tomoki108@users.noreply.github.com>
2019-01-23 12:01:01 +0800

    Make admin.addCategory page (front and back end)  (#6)
    * make admin_add_category page
    
    * fix for requests
    
    * fix for requests #2

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table = 'category';
+    
+    public $timestamps = false;
+
+    public function create($request)
+    {
+        $this->title = $request->title;
+        $this->description = $request->description;
+        $this->save();
+        return $this;
+    }
 }
