cb9872071bbdda3e4b376f57646b4e607efa58c1
Tomoki108 <shinonome81@gmail.com>
2019-01-29 04:21:31 +0900

    fix delete routing

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -13,23 +13,27 @@
 
     <section>
         <table class="table table-hover">
-          <thead>
-              <tr><th>Title</th><th>Description</th><th>Action</th></tr>
-          </thead>
+            <thead>
+                <tr><th>Title</th><th>Description</th><th>Action</th></tr>
+            </thead>
 
-          <tbody>
-              @foreach ($categories as $category)
-                  <tr>
-                      <td><a href="{{ action('WordController@index', $category) }}">{{$category->title}}</a></td>
-                      <td>{{$category->description}}</td>
-                      <td>
-                          <a href="{{ action('WordController@add', $category) }}">Add word</a> | 
-                          <a href="{{ action('CategoryController@edit', $category) }}">Edit</a> | 
-                          <a href="{{ action('CategoryController@delete', $category) }}" onclick="return confirm('Are you sure?')">Delete</a>
-                      </td>
-                  </tr>
-               @endforeach
-          </tbody>
+            <tbody>
+                @foreach ($categories as $category)
+                    <tr>
+                        <td><a href="{{ action('WordController@index', $category) }}">{{$category->title}}</a></td>
+                        <td>{{$category->description}}</td>
+                        <td>
+                            <form action="{{ action('CategoryController@delete', $category) }}" method="post" class="form-inline">
+                                {{ method_field('delete') }}
+                                {{ csrf_field() }}
+                                <a href="{{ action('WordController@add', $category) }}">Add word&nbsp;</a>|
+                                <a href="{{ action('CategoryController@edit', $category) }}">&nbsp;Edit&nbsp;</a>|
+                                <button type="submit" class="btn btn-link p-0 m-0" onclick="return confirm('Are you sure?')">Delete</button>                               
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
         </table>
         <span class="float-right">{{ $categories->links() }}</span>
     </section>
