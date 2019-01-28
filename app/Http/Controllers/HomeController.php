383372b494acc12a303da03d234474a4f4674a64
Tomoki108 <shinonome81@gmail.com>
2019-01-28 15:43:57 +0900

    make wordsLearned and profile edit page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Lesson;
+use App\LessonWord;
+use App\Word;
+use App\WordAnswer;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('home');
+    }
+
+    public function dashboard()
+    {
+        $user = Auth::user();
+        foreach ($user->lessons as $lesson){
+            $learned_words = isset($learned_words) ? count($lesson->lessonWords()->get())+$learned_words : count($lesson->lessonWords()->get());
+        }
+        return view('dashboard', ['user' => $user, 'learned_words' => $learned_words]);
+    }
+
+    public function wordsLearned()
+    {
+        $lessons = Auth::user()->lessons()->get();
+        $words = [];
+        $word_answers = [];
+
+        foreach ($lessons as $lesson) {
+            $lesson_words = LessonWord::where('lesson_id', $lesson->id)->get();
+                foreach ($lesson_words as $lesson_word) {
+                    array_push($words, Word::find($lesson_word->word_id)->content);
+                    array_push($word_answers, WordAnswer::find($lesson_word->word_answer_id)->content);
+                } 
+        }
+        
+        return view('wordsLearned', [
+            'user' => Auth::user(),
+            'words' => $words,
+            'word_answers' => $word_answers
+            ]);
     }
 }
